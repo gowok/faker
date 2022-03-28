84381d356957ef6c8c332d5b8bf23a223c3ef2c8
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-03-28 12:54:10 +0700

    feat(name): add fake female names

--- name/name.go
+++ name/name.go
@@ -14,7 +14,12 @@ func New() *fakename {
 	return &fakename{}
 }
 
-func (f *fakename) Male(opts ...NameOption) string {
+func (f *fakename) getRandomIndex(list []string) uint {
+	r := random.New()
+	return r.Uint(0, uint(len(list)-1))
+}
+
+func (f *fakename) FromFirstnamesAndLastnames(firstnames, lastnames []string, opts ...NameOption) string {
 	opt := &nameOption{
 		length: 2,
 	}
@@ -22,19 +27,24 @@ func (f *fakename) Male(opts ...NameOption) string {
 		o(opt)
 	}
 
-	r := random.New()
 	var name string
 	for i := uint(0); i < opt.length; i++ {
-		var nameIndex uint
 		if i == 0 {
-			nameIndex = r.Uint(0, uint(len(id.MaleFirstnames)-1))
-			name = id.MaleFirstnames[nameIndex]
+			nameIndex := f.getRandomIndex(firstnames)
+			name = firstnames[nameIndex]
 		} else {
-			nameIndex = r.Uint(0, uint(len(id.MaleLastnames)-1))
-			name = fmt.Sprintf("%s %s", name, id.MaleLastnames[nameIndex])
+			nameIndex := f.getRandomIndex(lastnames)
+			name = fmt.Sprintf("%s %s", name, lastnames[nameIndex])
 		}
-
 	}
 
 	return name
+}
+
+func (f *fakename) Male(opts ...NameOption) string {
+	return f.FromFirstnamesAndLastnames(id.MaleFirstnames, id.MaleLastnames, opts...)
+}
+
+func (f *fakename) Female(opts ...NameOption) string {
+	return f.FromFirstnamesAndLastnames(id.FemaleFirstnames, id.FemaleLastnames, opts...)
 }
--- name/name_test.go
+++ name/name_test.go
@@ -10,11 +10,20 @@ import (
 var Name = New()
 
 func TestName(t *testing.T) {
-	t.Run("can fake name with length", func(t *testing.T) {
+	t.Run("can fake male name with length", func(t *testing.T) {
 		must := must.New(t)
 		expected := uint(5)
 		result := Name.Male(WithLength(expected))
 		actual := uint(len(strings.Split(result, " ")))
 		must.Equal(expected, actual)
 	})
+
+	t.Run("can fake female name with length", func(t *testing.T) {
+		must := must.New(t)
+		expected := uint(5)
+		result := Name.Female(WithLength(expected))
+		actual := uint(len(strings.Split(result, " ")))
+		must.Equal(expected, actual)
+	})
+
 }

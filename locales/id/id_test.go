063ded3be32b783c9c65f1e0bd1a9ba7ae2db562
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-02-14 09:55:52 +0700

    test(id): 🧪 func Name

--- locales/id/id_test.go
+++ locales/id/id_test.go
@@ -14,4 +14,14 @@ func Test_New(t *testing.T) {
 		must.NotNil(id)
 	})
 
+	t.Run("has name", func(t *testing.T) {
+		name := id.Name()
+
+		must := must.New(t)
+		must.NotNil(name)
+		must.NotNil(name.FemaleFirstnames)
+		must.NotNil(name.FemaleLastnames)
+		must.NotNil(name.MaleFirstnames)
+		must.NotNil(name.MaleLastnames)
+	})
 }
